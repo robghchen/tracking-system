c39867fbbdb60c58a5575f1301b612b79e6b1a7d
Robert Chen <robert.rob.chen@gmail.com>
2020-05-09 21:28:54 -0400

    first assignment

--- src/App.tsx
+++ src/App.tsx
@@ -10,141 +10,82 @@ import './App.css';
 // o array
 // o object
 // o function
-// o helper methods
+// o helper methods (it's just another function)
 
-// Practice
-// create a new array called favoriteMovies with 5 movie objects with the keys of title and ranking
-// add the new favoriteMovies array to our info object
-// use the info object to render the titles of your favorite movies (all 5 of them) to chrome browser
-// with the info object's favoriteMovies array, use .forEach and if to only render the titles with a ranking greater 3. only your top two favorite movies should show up in chrome.
-// now use .map instead of .forEach to render your top two favorite movies but change the top ranking movie title to all caps
+// Exercise 1
+// a. inside render function, create a new array called recentMovies with 5 movie objects each with the keys 'title' and 'ranking', give them values of your choice
+
+// b. create a new object called aboutMe with the key and value of recentMovies array
+
+// c. create a helper method above render() called showAllMovies(), pass in the aboutMe object to render the titles of your recent movies (all 5 of them) to chrome browser with .map
+// Note: your movie titles are probably going to be jumbled together, try wrapping a <p> tag around it like so:
+// return <p>{movie.title}</p>
+
+// d. Comment out showAllMovies() where we call the function. Create another helper method called showTopTwoMovies(). This time pass in the aboutMe object's recentMovies array instead of the whole aboutMe object. Use a combination of .filter and .map to only render the titles with a ranking lower than 3.
+
+// e. In your showTopTwoMovies() function, inside of the .map, I want you to add an if condition to change the top ranking movie title to all caps. but how you ask? time to practice googling!
+
+// Vocabulary: 'refactor' is a word that is commonly used in programming, it means to adjust code
+// f. Stretch goal!! Refactor your showAllMovies() helper function so that you can use it inside of showTopTwoMovies() instead of doing .map
+// We will start Lesson 2 with the solution to this
+
+// Exercise 2
+// TBD
 
 class App extends React.Component {
-	amy = () => {
-		const hasCodedBefore = false // boolean
-		const occupation = 'sales' // string
-		const monthsCoded = 0 // number
-		const favoriteCharacters = ['Astrid', 'Hannabal Lector'] // array
-
-		const isIncludesAstrid = favoriteCharacters.includes('Astrid') // true or false
-
-		// favoriteCharacters.forEach(character => {
-		// 	console.log(character)
-		// })
-		//console.log('amy', isIncludesAstrid)
-		const info = {
-			hasAmyCodedBefore: hasCodedBefore,
-			amyOccupation: occupation,
-		}
-		console.log(info)
-
-		return hasCodedBefore
-	}
-
-	wesley = () => {
-		const hasCodedBefore = true
-		const occupation = 'unemployed'
-		const monthsCoded = 30
-		const favoriteCharacters = ['W', '7', 'Hello World']
-		const isIncludesW = favoriteCharacters.includes('W')
-
-		// favoriteCharacters.forEach(character => {
-		// 	console.log(character)
-		// })
-		//console.log('wesley', isIncludesW)
-
-		const info = {
-			hasWesCodedBefore: hasCodedBefore,
-			wesOccupation: occupation,
-		}
-
-		const infoHasWes = info.hasWesCodedBefore
-		console.log({ infoHasWes })
-
-		return hasCodedBefore
-
-	}
-
-	rob = (hero) => {
-		const hasCodedBefore = true
-		const occupation = 'software eng'
-		const monthsCoded = 12
-		// const favoriteCharacters = ['spiderman', 'W']
-		// const aaaaaaa = 'a'
-		// const isIncludesW = favoriteCharacters.includes('000')
-		// favoriteCharacters.forEach(character => {
-		// 	// do stuff here
-		// 	console.log(character)
-		// })
-		const info = {
-			hasRobCodedBefore: hasCodedBefore,
-			robsOccupation: occupation,
-			favoriteCharacters: [
-				{
-					name: 'spiderman',
-					color: 'red',
-					isCool: false
-				},
-				{
-					name: 'batman',
-					color: 'grey',
-					isCool: false
-				},
-				{
-					name: 'thePenguin',
-					isVillain: true,
-					isCool: true
-				},
-				{
-					name: 'hulk',
-					isVillain: false,
-					realName: 'Bruce Banner',
-					isCool: true
-				},
-				{
-					name: 'drStrange',
-					isVallain: false,
-					isCool: true
-				}
-			],
-		}
-
-		const isHeroFound = info.favoriteCharacters.some(character => character.name === hero)
-
-		if (isHeroFound) {
-			return hero
-		} else {
-			return 'Hero not found'
-		}
-	}
-
 	render() {
-		const hero = 'spiderlady'
-		const villain = 'venom'
-
-		const robFavChars = this.rob(hero) // spiderman
-		console.log('robFavChars:', robFavChars)
-		const robsLeastFavChar = this.rob(villain) // vemon
-
 		return (
 			<div className="App" >
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						{}
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-			</a>
+					Call your helper methods here
 				</header>
 			</div >
 		);
 	}
 }
+
+// class App extends React.Component {
+// 	showAllMovies = (aboutMe) => {
+// 		return aboutMe.recentMovies.map(movie => {
+// 			return <p>{movie.title}</p>
+// 		})
+// 	}
+
+// 	showTopTwoMovies = (recentMovies) => {
+// 		const topTwoMovies = recentMovies.filter(movie => movie.ranking < 3)
+
+// 		return topTwoMovies.map(movie => {
+// 			if (movie.ranking === 1) {
+// 				return <p>{movie.title.toUpperCase()}</p>
+// 			}
+
+// 			if (movie.ranking === 2) {
+// 				return <p>{movie.title}</p>
+// 			}
+// 		})
+// 	}
+
+// 	render() {
+// 		const recentMovies = [
+// 			{ title: 'The Greatest Showman', ranking: 1 },
+// 			{ title: 'Train To Busan', ranking: 2 },
+// 			{ title: 'Knives Out', ranking: 3 },
+// 			{ title: 'A Star is Born', ranking: 4 },
+// 			{ title: 'Mission Impossible Fallout', ranking: 5 },
+// 		]
+// 		const aboutMe = {
+// 			recentMovies
+// 		}
+
+// 		return (
+// 			<div className="App" >
+// 				<header className="App-header">
+// 					{/* {this.showAllMovies(aboutMe)} */}
+// 					{this.showTopTwoMovies(aboutMe.recentMovies)}
+// 				</header>
+// 			</div >
+// 		);
+// 	}
+// }
 
 export default App;
