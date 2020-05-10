5013506c1ca04ae00b7d3b7567b1311d816de9f5
Robert Chen <robert.rob.chen@gmail.com>
2020-05-10 13:06:11 -0400

    solutions to exercise 2 and 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
@@ -31,7 +30,7 @@ import './App.css';
 // We will start Lesson 2 with the solution to this
 
 // Exercise 2 - Get comfortable with if conditions using && and ||
-// a. Make a new array called petsIWant with 10 objects with the key value pair of 'animal: string', 'isFluffy: boolean', 'isFly: boolean', 'isExtinct: boolean'
+// a. Make a new array called petsIWant with 10 objects with the key value pair of 'animal: string', 'isFluffy: boolean', 'canFly: boolean', 'isExtinct: boolean'
 
 // b. Add petsIWant array to aboutMe object
 
@@ -45,11 +44,13 @@ import './App.css';
 // d. Create a helper method that renders only the pets that are not fluffy and cannot fly or is extinct
 // Note: use parenthesis to indicate order of operations, exactly like math (5 + 1) / 2
 
-// e. Create a helper method that renders animals with a length greater than 5 or cannot fly or is fluffy
+// e. Create a helper method that renders animals with a length greater than 10 or cannot fly or is fluffy
 
-// Exercise 3 - Put it all together
+// Exercise 3 - Putting it all together. This is extra hard, don't feel discouraged if you don't complete this, we can go over this together
 // a. Add keys 'wouldWatchAgain: boolean', 'wouldRecommendToGrandma: boolean', and 'animalsInMovie: array[string]' to each recent movie
-// make it up for the animalsInMovie array
+// make it up for the animalsInMovie array, use the animals that are from your petsIWant array
+
+// Hint: You are going to need to use combinations of loops and they will be nested together
 
 // b. Render only the movies that you would watch again or has animals that can fly
 
@@ -57,7 +58,7 @@ import './App.css';
 
 // d. Render only the pets that are in a movie
 
-// e. Render the pets that are extinct and pets that are in movies that you would not watch again
+// e. Render the pets that are extinct and the pets that are in movies that you would not watch again
 
 
 class App extends React.Component {
@@ -95,23 +96,123 @@ class App extends React.Component {
 // 		})
 // 	}
 
+// 	showFluffyOrCanFlyPets = (pets) => {
+// 		return pets.map(pet => {
+// 			if (pet.isFluffy || pet.canFly) {
+// 				return <p>{pet.animal}</p>
+// 			}
+// 		})
+// 	}
+
+// 	showNotFluffyAndCannotFlyOrIsExtinct = (pets) => {
+// 		return pets.map(pet => {
+// 			if ((!pet.isFluffy && !pet.canFly) || pet.isExtinct) {
+// 				return <p>{pet.animal}</p>
+// 			}
+// 		})
+
+// 		// you can also do filter first if you want, there are many ways to acheive the same thing in code
+
+// 		// const filteredPets = pets.filter(pet => {
+// 		// 	return (!pet.isFluffy && !pet.canFly) || pet.isExtinct
+// 		// })
+
+// 		// return filteredPets.map(pet => {
+// 		// 	return <p>{pet.animal}</p>
+// 		// })
+// 	}
+
+// 	showLongAnimalsOrCannotFlyOrIsFluffy = (pets) => {
+// 		return pets.map(pet => {
+// 			if (pet.animal.length > 10 || !pet.canFly || pet.isFluffy) {
+// 				return <p>{pet.animal}</p>
+// 			}
+// 		})
+// 	}
+
+// 	showMoviesWouldWatchAgainOrHasAnimalsThatCanFly = (movies, pets) => {
+// 		return movies.map(movie => {
+// 			const hasAnimalsThatCanFly = movie.animalsInMovie.some(animal => {
+// 				return pets.some(pet => pet.animal === animal && pet.canFly)
+// 			})
+
+// 			if (movie.wouldWatchAgain || hasAnimalsThatCanFly) {
+// 				console.log('movie:', movie)
+// 				return <p>{movie.title}</p>
+// 			}
+// 		})
+// 	}
+
+// 	showMoviesGrandmaAndFluffyOrTopTwo = (movies, pets) => {
+// 		return movies.map(movie => {
+// 			const hasAnimalsThatAreFluffy = movie.animalsInMovie.some(animal => {
+// 				return pets.some(pet => pet.animal === animal && pet.isFluffy)
+// 			})
+
+// 			if (movie.wouldRecommendToGrandma && hasAnimalsThatAreFluffy || movie.ranking <= 2) {
+// 				return <p>{movie.title}</p>
+// 			}
+// 		})
+// 	}
+
+// 	showPetCelebs = (movies, pets) => {
+// 		return pets.map(pet => {
+// 			const isInAMovie = movies.some(movie => movie.animalsInMovie.includes(pet.animal))
+
+// 			if (isInAMovie) {
+// 				return <p>{pet.animal}</p>
+// 			}
+// 		})
+// 	}
+
+// 	showExtinctPetsAndPetsIWouldNotWatchAgain = (movies, pets) => {
+// 		return pets.map(pet => {
+// 			const isInMovieIWouldNotWatchAgain = movies.some(movie => {
+// 				return movie.animalsInMovie.includes(pet.animal) && !movie.wouldWatchAgain
+// 			})
+// 			if (pet.isExtinct || isInMovieIWouldNotWatchAgain) {
+// 				return <p>{pet.animal}</p>
+// 			}
+// 		})
+// 	}
+
 // 	render() {
 // 		const recentMovies = [
-// 			{ title: 'The Greatest Showman', ranking: 1 },
-// 			{ title: 'Train To Busan', ranking: 2 },
-// 			{ title: 'Knives Out', ranking: 3 },
-// 			{ title: 'A Star is Born', ranking: 4 },
-// 			{ title: 'Mission Impossible Fallout', ranking: 5 },
+// 			{ title: 'The Greatest Showman', ranking: 1, wouldWatchAgain: true, wouldRecommendToGrandma: true, animalsInMovie: ['squirrel', 'pug', 'sabertooth'] },
+// 			{ title: 'Train To Busan', ranking: 2, wouldWatchAgain: false, wouldRecommendToGrandma: false, animalsInMovie: ['bear', 'pterodactyl', 'panda'] },
+// 			{ title: 'Knives Out', ranking: 3, wouldWatchAgain: true, wouldRecommendToGrandma: false, animalsInMovie: ['mammoth', 'iguana', 'pug', 'panther'] },
+// 			{ title: 'A Star is Born', ranking: 4, wouldWatchAgain: false, wouldRecommendToGrandma: true, animalsInMovie: ['sabertooth', 'bear', 'pigeon', 'panda'] },
+// 			{ title: 'Mission Impossible Fallout', ranking: 5, wouldWatchAgain: false, wouldRecommendToGrandma: false, animalsInMovie: ['pug', 'panther', 'bear', 'pigeon', 'pterodactyl'] },
+// 		]
+// 		const petsIWant = [
+// 			{ animal: 'panda', isFluffy: true, canFly: false, isExtinct: false },
+// 			{ animal: 'squirrel', isFluffy: false, canFly: false, isExtinct: false },
+// 			{ animal: 'bear', isFluffy: true, canFly: false, isExtinct: false },
+// 			{ animal: 'mammoth', isFluffy: true, canFly: false, isExtinct: true },
+// 			{ animal: 'sabertooth', isFluffy: true, canFly: false, isExtinct: true },
+// 			{ animal: 'pterodactyl', isFluffy: false, canFly: true, isExtinct: true },
+// 			{ animal: 'iguana', isFluffy: false, canFly: false, isExtinct: false },
+// 			{ animal: 'pigeon', isFluffy: false, canFly: true, isExtinct: false },
+// 			{ animal: 'pug', isFluffy: false, canFly: false, isExtinct: false },
+// 			{ animal: 'panther', isFluffy: true, canFly: false, isExtinct: false },
 // 		]
 // 		const aboutMe = {
-// 			recentMovies
+// 			recentMovies,
+// 			petsIWant
 // 		}
 
 // 		return (
 // 			<div className="App" >
 // 				<header className="App-header">
 // 					{/* {this.showAllMovies(aboutMe)} */}
-// 					{this.showTopTwoMovies(aboutMe.recentMovies)}
+// 					{/* {this.showTopTwoMovies(aboutMe.recentMovies)} */}
+// 					{/* {this.showFluffyOrCanFlyPets(aboutMe.petsIWant)} */}
+// 					{/* {this.showNotFluffyAndCannotFlyOrIsExtinct(aboutMe.petsIWant)} */}
+// 					{/* {this.showLongAnimalsOrCannotFlyOrIsFluffy(aboutMe.petsIWant)} */}
+// 					{/* {this.showMoviesWouldWatchAgainOrHasAnimalsThatCanFly(aboutMe.recentMovies, aboutMe.petsIWant)} */}
+// 					{/* {this.showMoviesGrandmaAndFluffyOrTopTwo(aboutMe.recentMovies, aboutMe.petsIWant)} */}
+// 					{/* {this.showPetCelebs(aboutMe.recentMovies, aboutMe.petsIWant)} */}
+// 					{/* {this.showExtinctPetsAndPetsIWouldNotWatchAgain(aboutMe.recentMovies, aboutMe.petsIWant)} */}
 // 				</header>
 // 			</div >
 // 		);
