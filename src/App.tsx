2de818c1ac946a32c0f76c134b21b088f6eeda62
Robert Chen <robert.rob.chen@gmail.com>
2020-05-16 11:30:42 -0400

    show all users and the jobs they applied to

--- src/App.tsx
+++ src/App.tsx
@@ -2,136 +2,130 @@ import React from 'react';
 import './App.css';
 
 
-// Lesson 1 - Javascript
-// o boolean
-// o string
-// o number
-// o array
-// o object
-// o function
-// o helper methods (it's just another function)
+// Lesson 2 - React
+// - JSON server
+// - async / await
+// - state
+// - props
+// - component life cycle
+// - react router
+// - start building the frontend
 
 class App extends React.Component {
-	getRedCharacters = (info) => {
-		const redCharacters = []
-
-		info.favoriteCharacters.forEach(char => {
-			if (char.color === 'red') {
-				redCharacters.push(char.name)
-			}
+	showJobsForUsers = (users) => {
+		return users.map(user => {
+			return user.jobs.map(job => {
+				return <div>
+					<p>{user.email}</p>
+					<p>{job.title}</p>
+					<p>{job.salary}</p>
+					<p>{job.location}</p>
+					<p>{job.description}</p>
+				</div>
+			})
 		})
-
-		return redCharacters
-	}
-
-	allNotCool = (info) => {
-		const updatedCharacters = info.favoriteCharacters.map(char => {
-			let updatedChar = char
-
-			updatedChar.isCool = false;
-
-			return updatedChar
-		})
-
-		return updatedCharacters
-	}
-
-	isABruce = (character) => {
-		return character.realName.includes('Bruce')
-	}
-
-	findCharacter = (info, trueOrFalse) => {
-		const character = info.favoriteCharacters.find(char => char.isVillain === trueOrFalse)
-
-		return character
-	}
-
-	isCharacterHere = (info, name) => {
-		return info.favoriteCharacters.some(char => char.name === name)
-	}
-
-	getVillains = (info) => {
-		return info.favoriteCharacters.filter(char => char.isVillain)
 	}
 
 	render() {
-		const info = {
-			hasCodedBefore: true,
-			occupation: 'software engineer',
-			favoriteCharacters: [
-				{
-					name: 'spiderman',
-					color: 'red',
-					isVillain: false,
-					realName: 'Peter Parker',
-					isCool: false
-				},
-				{
-					name: 'batman',
-					color: 'grey',
-					isVillain: false,
-					realName: 'Bruce Wayne',
-					isCool: false
-				},
-				{
-					name: 'joker',
-					color: 'purple',
-					isVillain: true,
-					realName: 'Jack Napier',
-					isCool: true
-				},
-				{
-					name: 'thePenguin',
-					color: 'white',
-					isVillain: true,
-					realName: 'idk',
-					isCool: true
-				},
-				{
-					name: 'hulk',
-					color: 'green',
-					isVillain: false,
-					realName: 'Bruce Banner',
-					isCool: true
-				},
-				{
-					name: 'drStrange',
-					color: 'red',
-					isVallain: false,
-					realName: 'Benedict Cumberbatch',
-					isCool: true
-				}
-			],
-		}
-
-		const redCharacters = this.getRedCharacters(info)
-		console.log('redCharacters:', redCharacters)
-
-		const notCool = this.allNotCool(info)
-		console.log('notCool:', notCool)
-
-		const isFirstCharacterBruce = this.isABruce(info.favoriteCharacters[0])
-		console.log('isFirstCharacterBruce:', isFirstCharacterBruce)
-
-		const isSecondCharacterBruce = this.isABruce(info.favoriteCharacters[1])
-		console.log('isSecondCharacterBruce:', isSecondCharacterBruce)
-
-		const findFirstVillain = this.findCharacter(info, true)
-		console.log('findVillain:', findFirstVillain)
-
-		const findFirstNonVillain = this.findCharacter(info, false)
-		console.log('findFirstNonVillain:', findFirstNonVillain)
-
-		const isCharacterHere = this.isCharacterHere(info, 'drStrange')
-		console.log('isCharacterHere:', isCharacterHere)
-
-		const getVillains = this.getVillains(info)
-		console.log('getVillains:', getVillains)
+		const users = [
+			{
+				email: "wesley@gmail.com",
+				jobs: [
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+				]
+			},
+			{
+				email: "amy@gmail.com",
+				jobs: [
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					}
+				]
+			},
+			{
+				email: "kj@gmail.com",
+				jobs: [
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					}
+				]
+			},
+			{
+				email: "rob@gmail.com",
+				jobs: [
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					},
+					{
+						title: "software engineer",
+						salary: 150000,
+						location: "new york",
+						description: "a bunch of stuff",
+					}
+				]
+			},
+		]
 
 		return (
 			<div className="App" >
 				<header className="App-header">
-					Right click in browser to inspect or cmd + option + j
+					{this.showJobsForUsers(users)}
 				</header>
 			</div >
 		);
