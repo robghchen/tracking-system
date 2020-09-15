009b20634c908d78884a019f5c893c174ee92a48
Robert Chen <robert.rob.chen@gmail.com>
2020-09-15 09:45:36 -0400

    feat(loader): return loader if no current user

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import UserUpdate from './components/UserUpdate'
 import { Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import DashboardContainer from './containers/DashboardContainer'
+import { Dimmer, Loader } from 'semantic-ui-react'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
@@ -70,145 +71,12 @@ class App extends React.Component<AppProps, AppState> {
 	render() {
 		let { currentUser } = this.state
 
-		if (!currentUser) { // faking currentUser because we didn't create a login, otherwise this would be a div that says "Loading..."
-			currentUser = {
-				"id": 1,
-				"email": "wesley@gmail.com",
-				"jobs": [
-					{
-						"id": 1,
-						"title": "frontend engineer",
-						"salary": 150000,
-						"rating": 4,
-						"location": "new york",
-						"description": "a bunch of stuff",
-						"companyName": "Google",
-						"companySize": 46980,
-						"industry": "Tech",
-						"status": "in person interview"
-					},
-					{
-						"id": 2,
-						"title": "backend engineer",
-						"salary": 180000,
-						"rating": 3,
-						"location": "california",
-						"description": "a bunch of stuff",
-						"companyName": "Ray Bans",
-						"companySize": 15585,
-						"industry": "Glasses",
-						"status": "phone interview"
-					},
-					{
-						"id": 3,
-						"title": "Engineer",
-						"salary": 120000,
-						"rating": 1,
-						"location": "New York",
-						"description": "car stuff",
-						"companyName": "Nissan",
-						"companySize": 2000,
-						"industry": "vehicle",
-						"status": "offer"
-					},
-					{
-						"id": 4,
-						"title": "frontend engineer",
-						"salary": 150000,
-						"rating": 5,
-						"location": "new york",
-						"description": "a bunch of stuff",
-						"companyName": "Microsoft",
-						"companySize": 46980,
-						"industry": "Tech",
-						"status": "in person interview"
-					},
-					{
-						"id": 5,
-						"title": "backend engineer",
-						"salary": 180000,
-						"rating": 3,
-						"location": "california",
-						"description": "a bunch of stuff",
-						"companyName": "AirBnB",
-						"companySize": 15585,
-						"industry": "Glasses",
-						"status": "technical interview"
-					},
-					{
-						"id": 6,
-						"title": "Engineer",
-						"salary": 120000,
-						"rating": 2,
-						"location": "New York",
-						"description": "car stuff",
-						"companyName": "BMW",
-						"companySize": 2000,
-						"industry": "vehicle",
-						"status": "technical interview"
-					},
-					{
-						"id": 7,
-						"title": "frontend engineer",
-						"salary": 150000,
-						"rating": 5,
-						"location": "new york",
-						"description": "a bunch of stuff",
-						"companyName": "Netflix",
-						"companySize": 46980,
-						"industry": "Tech",
-						"status": "in person interview"
-					},
-					{
-						"id": 8,
-						"title": "backend engineer",
-						"salary": 180000,
-						"rating": 1,
-						"location": "california",
-						"description": "a bunch of stuff",
-						"companyName": "Ray Bans",
-						"companySize": 15585,
-						"industry": "Hulu",
-						"status": "phone interview"
-					},
-					{
-						"id": 9,
-						"title": "Engineer",
-						"salary": 120000,
-						"rating": 4,
-						"location": "New York",
-						"description": "car stuff",
-						"companyName": "Glassdoor",
-						"companySize": 2000,
-						"industry": "vehicle",
-						"status": "phone interview"
-					},
-					{
-						"id": 10,
-						"title": "Engineer",
-						"salary": 120000,
-						"rating": 4,
-						"location": "New York",
-						"description": "car stuff",
-						"companyName": "Mercedes",
-						"companySize": 2000,
-						"industry": "vehicle",
-						"status": "technical interview"
-					},
-					{
-						"id": 11,
-						"title": "Engineer",
-						"salary": 120000,
-						"rating": 5,
-						"location": "New York",
-						"description": "car stuff",
-						"companyName": "Tesla",
-						"companySize": 2000,
-						"industry": "vehicle",
-						"status": "offer"
-					}
-				]
-			}
+		if (!currentUser) {
+			return (
+				<Dimmer active>
+					<Loader />
+				</Dimmer>
+			)
 		}
 
 		return (
